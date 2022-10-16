5372572b7402ec21c88ad6f0b3c3db0d7643bc9c
Manav Agarwal <manavagarwal1410@gmail.com>
2022-10-16 11:49:22 +0530

    project cards navigate

--- codebuddy/src/components/ProjectCard/index.js
+++ codebuddy/src/components/ProjectCard/index.js
@@ -1,4 +1,5 @@
 import {SkillTag} from '../skillTag/index'
+import {useNavigate} from "react-router-dom";
 
 export const ProjectCardSmall = ({
     projectData={}
@@ -26,23 +27,26 @@ export const ProjectCardSmall = ({
 export const ProjectCardWide = ({
   projectData = {}
 }) => {
+  const navigate = useNavigate();
 
   return(
-    <div className="w-full mt-4 bg-dark-200 px-5 py-6 pb-10 rounded-lg border border-[#30363D]">
-        <div className="w-full flex text-lg dark:text-white font-bold">
-          <p className="pr-1">
-              {projectData.name? projectData.name : "Project Title"}
-          </p>
-        </div>
-        <div className="w-full flex text-sm dark:text-light-200">
-          {projectData.description? projectData.description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-        </div>
-        <div className="w-full flex gap-1 mt-3">
-        {
-          projectData.tags?.length > 0 
-          ? projectData.tags.map((tag,i) => <SkillTag tagValue={tag} key={i}/>)
-          : ['NodeJS','CSS','React','NextJS'].map((tag,i) => <SkillTag variant={'sm'} tagValue={tag} key={i}/>)
-        }
+    <div onClick={() => navigate(`${projectData.id?`/project/${projectData.id}`:'404'}`)} className="cursor-pointer">
+      <div className="w-full mt-4 bg-dark-200 px-5 py-6 pb-10 rounded-lg border border-[#30363D]">
+          <div className="w-full flex text-lg dark:text-white font-bold">
+            <p className="pr-1">
+                {projectData.name? projectData.name : "Project Title"}
+            </p>
+          </div>
+          <div className="w-full flex text-sm dark:text-light-200">
+            {projectData.description? projectData.description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+          </div>
+          <div className="w-full flex gap-1 mt-3">
+          {
+            projectData.tags?.length > 0 
+            ? projectData.tags.map((tag,i) => <SkillTag tagValue={tag} key={i}/>)
+            : ['NodeJS','CSS','React','NextJS'].map((tag,i) => <SkillTag variant={'sm'} tagValue={tag} key={i}/>)
+          }
+          </div>
         </div>
       </div>
   )
